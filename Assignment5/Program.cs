68da1911e4648c1fba77270adde86e484d812db0
sk4yb3n <sk4yb3n@hotmail.com>
2017-05-20 13:28:16 +0200

    initial

---
+++ Assignment5/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Chrome;
+
+namespace Assignment5
+{
+    class Program
+    {
+        private static string Url = "http://linkedin.com";
+
+
+        static void Main(string[] args)
+        {
+            var driver = new ChromeDriver();
+            driver.Navigate().GoToUrl(Url);
+
+            var username = driver.FindElementById("login-email");
+            var password = driver.FindElementById("login-password");
+            var loginBtn = driver.FindElementById("login-submit");
+            
+
+
+            username.SendKeys("sk4yb3n@gmail.com");
+            password.SendKeys("Ikbhsiqlfg123");
+            loginBtn.Click();
+
+            driver.Navigate().Refresh();
+            var logoutEl = driver.FindElementByClassName("nav-item__dropdown-trigger--title");
+
+            if (!logoutEl.Text.Equals("Me"))
+            {
+                Console.WriteLine("Ne valja");
+            }
+
+        }
+    }
+}
---
+++ SeleniumTest/UnitTest1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private static string Url = "http://linkedin.com";
+
+        [TestMethod]
+        public void LoginVerification() 
+        {
+            var driver = new ChromeDriver();
+            driver.Navigate().GoToUrl(Url);
+
+            var username = driver.FindElementById("login-email");
+            var password = driver.FindElementById("login-password");
+            var loginBtn = driver.FindElementById("login-submit");
+
+            username.SendKeys("email");
+            password.SendKeys("pass");
+            loginBtn.Click();
+
+            driver.Navigate().Refresh();
+
+            var logoutEl = driver.FindElementByClassName("nav-item__dropdown-trigger--title");
+            var check = false;
+
+            if (logoutEl != null)
+            {
+                if (!logoutEl.Text.Equals("Me"))
+                {
+                    check = true;
+                }
+            }
+
+            Assert.Equals(check, true);
+        }
+    }
+}
